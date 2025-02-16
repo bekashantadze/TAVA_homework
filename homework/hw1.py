b8a25bd795273c60693856a8a5dbff719d0e7599
bekashantadze <bekashantadze10@gmail.com>
2025-02-16 20:39:46 +0400

    second commit

---
+++ homework/hw1.py
@@ -0,0 +1,11 @@
+name= "Beqa"
+
+age=15
+
+height=1.71
+
+hobby= "football"
+
+live="khulo"
+
+print("me var " + name + " var " + str(age) + " wlis " + " Chemi simaglea " + str(height) + " chemi hobby aris " + hobby + " Chemi sacxovrebeli adgilia " + str(live))
