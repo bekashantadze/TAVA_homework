6a1b54d8a2c6d36075474beea2e1dc88c5a07be1
bekashantadze <bekashantadze10@gmail.com>
2025-02-22 22:23:15 +0400

    second commit

---
+++ day01/classwork/input.py
@@ -0,0 +1,7 @@
+#  მომხმარებელს input-ის გამოყენებით უნდა შემოატანინოთ მისი სახელი და გვარი შემოტანის შემდეგ უნდა მიესალმოთ მას 
+name=input( "whats your name: ")
+surname=input( "whats your surname: ")
+print("hay" + " " + name + surname)
+#  მომხმარებელს input-ის გამოყენებით უნდა შემოატანინოთ 2 რიცხვი და შემდეგ ამ 2 რიცხვი უნდა გააკეთოთ მათემატიკური მაგალითები/ოპერაციები
+num1_1=int(input("your first name: "))
+num2_2=int(input ( "your second name: "))
